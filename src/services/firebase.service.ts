7f3bb4a7b2c8d1c2e26455bbb5e80993877e8640
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2025-09-02 01:14:43 +0600

    update firebase service

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -6,7 +6,7 @@ export const initializeFirebase = async () => {
   try {
     const firebaseConfig: admin.ServiceAccount = {
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n") : undefined,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL
     };
 
